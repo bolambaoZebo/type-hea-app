4fedd77d18256944b0582dc38c9bce373d946097
dnamicro <bolambaoz@gmail.com>
2021-02-11 00:08:12 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import TypeHead from './TypeHead';
+
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <TypeHead
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
